e87b877ab0dfd2549c490be81456dc8a6c2bfc31
Shahmeer Khan <101963974+shahmeer07@users.noreply.github.com>
2024-02-21 15:43:09 +0500

    Now showing related records and its line fields

--- CS_Customsalesorder.js
+++ CS_Customsalesorder.js
@@ -6,28 +6,65 @@
 define(['N/record','N/ui/dialog'] , function (record,dialog){
 
     function ShowRelatedRecordDialog(){
-        console.log("Button clicked!")
+        try{
+            console.log("Button clicked!")
         var salesOrderId = Number(location.search.match(/id=([^&]+)/)[1])
         var salesOrder = record.load({
             type: record.Type.SALES_ORDER,
-            id: salesOrderId
+            id: salesOrderId,
+            isDynamic: true
         })
-        var customerId = salesOrder.getValue({ fieldId: 'entity' })
-        var customername = salesOrder.getText({ fieldId : 'entity'})
-        var customertype = salesOrder.getValue({ fieldId : 'isperson'})
-        var customeremail = salesOrder.getValue({ fieldId:'email'})
-        var salesrep = salesOrder.getText({fieldId: 'salesrep'})
+        var linecount = salesOrder.getLineCount({ sublistId : 'links'})
+        var dialogMessage = ' '
+        for (var i=0 ; i < linecount ; i++){
+            var relatedrecordType = salesOrder.getSublistText({
+                sublistId : 'links',
+                fieldId: 'type',
+                line: i 
+            }) || 'empty'
+            var relatedrecordnumber = salesOrder.getSublistValue({
+                sublistId : 'links',
+                fieldId: 'tranid',
+                line: i
+            }) || 'empty'
 
-        var dialogMessage = 'Customer ID : ' + customerId + '\n' +
-                        'Customer Name : ' + customername + '\n' +
-                        'Customer Type : ' + customertype + '\n' +
-                        'Customer Email : ' + customeremail + '\n' +
-                        'Sales Representative : ' + salesrep  ; 
+            var relatedrecordstatus = salesOrder.getSublistValue({
+                sublistId : 'links',
+                fieldId: 'status',
+                line: i
+            }) || 'empty'
+
+            
+            var relatedrecorddate = salesOrder.getSublistValue({
+                sublistId : 'links',
+                fieldId: 'trandate',
+                line: i
+            }) || 'empty'
+
+            dialogMessage +='Related Records Type: ' + relatedrecordType + ' , ' +  '\n' +  'Related Record date: ' + relatedrecorddate + ' , ' +  '\n' +  'Related records number : ' + relatedrecordnumber  +  ' , '  + '\n' +  'Related records status : ' +  '\n' +  relatedrecordstatus ;
+            
+            
+            
+
+            log.debug({
+                title: "line related records details: ",
+                details: dialogMessage
+            })
+        }
+
+         
 
         dialog.alert({
             title: "Related Records",
             message: dialogMessage
         })
+        }
+        catch(e){
+            log.error({
+                title: e.title,
+                details: e.message
+            })
+        }
     }
 
 
